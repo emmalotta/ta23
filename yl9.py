f9a7186e0db0b51c4ab392627d535b54794d8d2e
Emma <emmalotta9@gmail.com>
2023-10-25 11:47:09 +0300

    murt

--- yl9.py
+++ yl9.py
@@ -1,7 +1,7 @@
 a = float(input("Sisestage kolmnurga esimese külje pikkus: "))
 b = float(input("Sisestage kolmnurga teise külje pikkus: "))
 c = float(input("Sisestage kolmnurga kolmanda külje pikkus: "))
-if a>0 and b>0 and c>0:
+if a < b + c or b < a + c or c < a + b:
     if a == b == c:
         print("Kolmnurk on võrdkülgne")
     elif a == b or a == c or b == c or b == a:
